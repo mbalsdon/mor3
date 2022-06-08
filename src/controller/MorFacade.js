90ef0dc4aeb16ebfcb575cefaf6ce2391cef3121
mbalsdon <m.mathewbalsdon@gmail.com>
2022-06-07 20:42:24 -0700

    half implement user top plays scrape algo

--- src/controller/MorFacade.js
+++ src/controller/MorFacade.js
@@ -73,20 +73,65 @@ module.exports = class MorFacade {
     })
   }
 
-  scrapeUserTopPlays () {
+  async scrapeUserTopPlays () {
     console.info('MorFacade::scrapeUserTopPlays()')
-    this.sheets.fetchUserIds()
-      .then((userIds) => {
-        for (const id of userIds) {
-          this.osu.fetchUserTopPlays(id)
-            .then((scores) => {
-              console.log(scores.map((s) => [s.user.username, s.beatmapset.title]))
-              // helper function parse relevant data
-              // put into dict (key=modcombo, val=sorted list by pp)
-              // remember to remove nf,so,sd,pf,etc.
-            })
+    const userIds = await this.sheets.fetchUserIds()
+    // Key = Mod string; Value = Array sorted by pp
+    const dict = {}
+    // Iterate over each user's top 100, putting each score into the dict
+    for (const id of userIds) {
+      const scores = await this.osu.fetchUserTopPlays(id)
+      for (const score of scores) {
+        const ps = this.parseScore(score)
+        const key = ps[0]
+        const formattedScore = ps.slice(1, ps.length)
+        // The key exists
+        if (Object.keys(dict).includes(key)) {
+          dict[key].push(formattedScore)
+        // The key doesn't exist
+        } else {
+          dict[key] = [formattedScore]
         }
+      }
+    }
+
+    for (const k of Object.keys(dict)) {
+      dict[k].sort((a, b) => {
+        return parseInt(a[4]) - parseInt(b[4])
       })
-    // { ... }
+    }
+    
+    // TODO: put the dict in sheet
+  }
+
+  // private
+  // takes a Score object (https://osu.ppy.sh/docs/index.html#score)
+  parseScore (s) {
+    return [
+      this.parseModKey([...s.mods]), // key for dict
+      `=HYPERLINK("https://osu.ppy.sh/users/${s.user.id}", ${s.user.username})`,
+      `=HYPERLINK("${s.beatmap.url}", "${s.beatmapset.artist} - ${s.beatmapset.title} [${s.beatmap.version}]")`,
+      (s.mods.length === 0) ? 'NM' : s.mods.join(), // turn the mods into a single string
+      Math.round(s.accuracy * 10000) / 100, // 0.xxxx => xx.xx
+      s.pp,
+      s.created_at
+    ]
+  }
+
+  // takes mods (array of str) from Score obj, returns "normalized form" (e.g. HDNC => HDDT; NF => NM)
+  parseModKey (mods) {
+    // NC => DT
+    if (mods.includes('NC')) mods.splice(mods.indexOf('NC'), 1, 'DT')
+    // NF / SO / SD / PF => remove it
+    if (mods.includes('NF')) mods.splice(mods.indexOf('NF'), 1)
+    if (mods.includes('SO')) mods.splice(mods.indexOf('SO'), 1)
+    if (mods.includes('SD')) mods.splice(mods.indexOf('SD'), 1)
+    if (mods.includes('PF')) mods.splice(mods.indexOf('PF'), 1)
+    // empty => NM
+    if (mods.length === 0) {
+      return 'NM'
+    } else {
+      return mods.join()
+    }
   }
 }
--- test/Tests.js
+++ test/Tests.js
@@ -3,4 +3,7 @@ const MorFacade = require('../src/controller/MorFacade')
 MorFacade.build()
   .then((facade) => {
     facade.scrapeUserTopPlays()
+      .then((response) => {
+        console.log()
+      })
   })
