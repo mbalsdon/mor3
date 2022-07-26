45e0a289551a70966a225e32a5aadf3bd6f6bff3
mbalsdon <m.mathewbalsdon@gmail.com>
2022-07-26 10:59:20 -0700

    move bot code into folder and put into a class for code consistency

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
-import 'dotenv/config'
-import { Client, GatewayIntentBits } from 'discord.js';
-import MorFacade from './src/controller/MorFacade.js';
+import Bot from './src/client/Bot.js'
 
-const facade = await MorFacade.build()
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.once('ready', () => {
-	console.log('MOR3 is online!');
-});
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const { commandName } = interaction;
-
-	if (commandName === 'ping') {
-		await interaction.reply('pong!');
-	} else if (commandName === 'echo') {
-		const input = interaction.options.getString('input')
-		await interaction.reply(input)
-	} else if (commandName === 'metadata') {
-		const metadata = await facade.getMetadata()
-		await interaction.reply(JSON.stringify(metadata))
-	}
-});
-
-// Must be the last line of code
-client.login(process.env.DISCORD_API_BOT_TOKEN)
+const client = await Bot.build()
+await client.start()
---
+++ src/client/Bot.js
@@ -0,0 +1,50 @@
+import 'dotenv/config'
+import { Client, GatewayIntentBits } from 'discord.js';
+import MorFacade from '../controller/MorFacade.js';
+
+export default class Bot {
+  #facade
+  #client
+
+  constructor (morFacade, botClient) {
+    if (typeof morFacade === 'undefined' || typeof botClient === 'undefined') {
+      throw new Error('Cannot be called directly')
+    }
+    this.#facade = morFacade
+    this.#client = botClient
+  }
+
+  static async build () {
+    console.info('Bot::build()')
+    const morFacade = await MorFacade.build()
+    const botClient = new Client({ intents: [GatewayIntentBits.Guilds] })
+    return new Bot(morFacade, botClient)
+  }
+
+  async start () {
+    console.info('Bot::start()')
+
+    this.#client.once('ready', () => {
+      console.log('MOR3 is online!')
+    })
+
+    this.#client.on('interactionCreate', async interaction => {
+      if (!interaction.isChatInputCommand()) return;
+    
+      const { commandName } = interaction;
+    
+      if (commandName === 'ping') {
+        await interaction.reply('pong!');
+      } else if (commandName === 'echo') {
+        const input = interaction.options.getString('input')
+        await interaction.reply(input)
+      } else if (commandName === 'metadata') {
+        const metadata = await this.#facade.getMetadata()
+        await interaction.reply(JSON.stringify(metadata))
+      }
+    });
+    
+    // Must be the last line of code
+    this.#client.login(process.env.DISCORD_API_BOT_TOKEN)
+  }
+}
