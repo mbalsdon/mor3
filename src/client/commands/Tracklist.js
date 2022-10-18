65c43fee006066743d5d46668e63058281aa2b85
mbalsdon <m.mathewbalsdon@gmail.com>
2022-10-17 18:22:43 -0700

    Remove spaces and add header

--- src/client/commands/Tracklist.js
+++ src/client/commands/Tracklist.js
@@ -3,9 +3,9 @@ import * as fs from 'fs'
 export default async function tracklistCmd (facade, interaction) {
   console.info('::tracklistCmd()')
   const users = await facade.getUsers()
-  let ret = ''
+  let ret = 'userID,username\n'
   for (let i = 0; i < users.length; i++) {
-    ret = ret.concat(`${users[i][0]}, ${users[i][1]}\n`)
+    ret = ret.concat(`${users[i][0]},${users[i][1]}\n`) // TODO: magic num
   }
   fs.writeFileSync('./tracklist.txt', ret)
   await interaction.reply({ files: ['./tracklist.txt'] })
