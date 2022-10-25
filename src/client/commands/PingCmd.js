0aad6e99ac42b915d55f627ad919cd8e303e1dd3
mbalsdon <m.mathewbalsdon@gmail.com>
2022-10-24 23:30:22 -0700

    Revert accidental addition of test code

--- src/client/commands/PingCmd.js
+++ src/client/commands/PingCmd.js
@@ -1,10 +1,6 @@
-import UserRow from "../../controller/User.js"
-
 export default async function pingCmd (interaction) {
   console.info('::pingCmd()')
   try {
-    const myUser = new UserRow(2, 'peppy', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 'https://google.com')
-    console.log(myUser.toArray())
     await interaction.reply('pong!')
   } catch (error) {
     await interaction.reply({ content: `\`\`\`${error.message}\nDM spreadnuts#1566 on Discord if you believe that this is a bug.\`\`\``, ephemeral: true })
