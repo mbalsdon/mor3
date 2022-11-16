ab8f97f7cb1ad0ebe069fb37a0cca72b4113e0ba
mbalsdon <m.mathewbalsdon@gmail.com>
2022-11-15 19:41:09 -0800

    Add scheduled task to index

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 import Bot from './src/client/Bot.js'
+import runScheduledJobs from './src/jobs/RunScheduledJobs.js'
+import * as cron from 'node-cron'
 
 await Bot.startBot()
+cron.schedule('0 */12 * * *', runScheduledJobs)
