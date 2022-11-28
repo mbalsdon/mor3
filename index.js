fcb53cbbb715313c9648c793ce17a4e32a7779b5
mbalsdon <m.mathewbalsdon@gmail.com>
2022-11-27 16:06:47 -0800

    Run jobs on absolute times rather than relative to bot start time

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ import * as cron from 'node-cron'
 
 const bot = await Bot.build()
 await bot.start()
-cron.schedule('0 0 */14 * *', createBackup)
-cron.schedule('0 */4 * * *', runScheduledJobs)
+cron.schedule('0 0 1,14 * *', createBackup)
+cron.schedule('0 0,4,8,12,16,20 * * *', runScheduledJobs)
