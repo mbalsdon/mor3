5c8d1a25cc8baa49f3ad50bb816dfb0a3bfc9057
mbalsdon <m.mathewbalsdon@gmail.com>
2022-11-27 16:48:38 -0800

    Add emojis to /submit and /unsubmit

--- src/client/commands/SubmitCmd.js
+++ src/client/commands/SubmitCmd.js
@@ -22,8 +22,8 @@ export default async function submitCmd (facade, interaction) {
       .setAuthor({ name: `Successfully added score to ${MorConfig.SHEETS.SPREADSHEET.NAME}:` })
       .setThumbnail(`${score.beatmapImgLink}`)
       .setDescription(`**[${score.beatmap}](https://osu.ppy.sh/scores/osu/${score.scoreId}) +${score.mods}** [${score.starRating}★]\n` +
-              `▸ **${score.pp}pp** ▸ ${score.accuracy}%\n` +
-              `▸ Set by [${score.username}](https://osu.ppy.sh/users/${score.userId}) on ${score.date}\n`)
+              `▸ :farmer: **${score.pp}pp** ▸ ${score.accuracy}%\n` +
+              `▸ :calendar_spiral: Set by [${score.username}](https://osu.ppy.sh/users/${score.userId}) on ${score.date}\n`)
       .setFooter({ text: `Last update: ${lastUpdated}` })
     await interaction.editReply({ embeds: [embed] })
   } catch (error) {
--- src/client/commands/UnsubmitCmd.js
+++ src/client/commands/UnsubmitCmd.js
@@ -22,8 +22,8 @@ export default async function unsubmitCmd (facade, interaction) {
       .setAuthor({ name: `Successfully removed score from ${MorConfig.SHEETS.SPREADSHEET.NAME}:` })
       .setThumbnail(`${score.beatmapImgLink}`)
       .setDescription(`**[${score.beatmap}](https://osu.ppy.sh/scores/osu/${score.scoreId}) +${score.mods}** [${score.starRating}★]\n` +
-              `▸ **${score.pp}pp** ▸ ${score.accuracy}%\n` +
-              `▸ Set by [${score.username}](https://osu.ppy.sh/users/${score.userId}) on ${score.date}\n`)
+              `▸ :farmer: **${score.pp}pp** ▸ ${score.accuracy}%\n` +
+              `▸ :calendar_spiral: Set by [${score.username}](https://osu.ppy.sh/users/${score.userId}) on ${score.date}\n`)
       .setFooter({ text: `Last update: ${lastUpdated}` })
     await interaction.editReply({ embeds: [embed] })
   } catch (error) {
