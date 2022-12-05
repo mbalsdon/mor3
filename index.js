6babe4e9c32c844c1d4e97bfc0673624defda4bd
mbalsdon <m.mathewbalsdon@gmail.com>
2022-12-04 21:41:42 -0800

    Tweak cron schedules

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ import * as cron from 'node-cron'
 
 const bot = await Bot.build()
 await bot.start()
-cron.schedule('0 0 1,14 * *', createBackup)
-cron.schedule('0 0,4,8,12,16,20 * * *', runScheduledJobs)
+cron.schedule('0 0 1,15 * *', createBackup)
+cron.schedule('0 0,8,16 * * *', runScheduledJobs)
