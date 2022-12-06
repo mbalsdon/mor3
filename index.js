c83b910e8324adf8b44ca66fa81aebefd8b97eb5
mbalsdon <m.mathewbalsdon@gmail.com>
2022-12-06 00:31:30 -0800

    Add comments to entrypoint

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import Bot from './src/client/Bot.js'
+
 import createBackup from './src/jobs/jobs/CreateBackup.js'
 import runScheduledJobs from './src/jobs/jobs/RunScheduledJobs.js'
 import JobHandler from './src/jobs/JobHandler.js'
 
+// Start discord bot
 const bot = await Bot.build()
 await bot.start()
 
+// Start scheduled jobs
 const jobs = {
   createBackup: ['0 0 1,15 * *', createBackup],
   runScheduledJobs: ['0 0,8,16 * * *', runScheduledJobs]
 }
-
 const jobHandler = new JobHandler(jobs)
 jobHandler.start()
