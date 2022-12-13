677d9a9a2a65a2621572255ce2dbc9d2b94a3fb5
mbalsdon <m.mathewbalsdon@gmail.com>
2022-12-12 23:09:34 -0800

    Update /help descriptions

--- src/client/commands/HelpCmd.js
+++ src/client/commands/HelpCmd.js
@@ -20,18 +20,18 @@ export default async function helpCmd (interaction) {
     const embed = new EmbedBuilder()
       .setColor(MorConfig.BOT_EMBED_COLOR)
       .setAuthor({ name: `${MorConfig.SHEETS.SPREADSHEET.NAME} Commands`, iconURL: MorConfig.SERVER_ICON_URL, url: 'https://github.com/mbalsdon/mor3' })
-      .setDescription('`help` - Documentation on the bot\'s commands\n' +
-            '`ping` - Checks if the bot is alive\n' +
-            `\`metadata\` - Displays ${MorConfig.SHEETS.SPREADSHEET.NAME} sheet metadata\n` +
-            '`users` - Displays list of tracked users, sorted by PP\n' +
-            '`user` - Displays a tracked user\'s stats\n' +
-            '`track` - Adds a user to be tracked [MODERATORS ONLY]\n' +
-            '`untrack` - Removes a user from being tracked [MODERATORS ONLY]\n' +
-            '`tracklist` - Lists all tracked users in a .txt file\n' +
-            '`submit` - Manually submits a score to the database\n' +
-            '`unsubmit` - Removes a submitted score from the database [MODERATORS ONLY]\n' +
-            '`scores` - Displays list of scores set by tracked users, sorted by PP\n' +
-            '`leaderboard` - Displays top plays on a map set by tracked users, sorted by PP\n')
+      .setDescription('`help` - List the bot\'s commands\n' +
+            '`ping` - Check if the bot is alive\n' +
+            `\`metadata\` - Display ${MorConfig.SHEETS.SPREADSHEET.NAME} sheet metadata\n` +
+            '`users` - Display leaderboard for currently tracked users \n' +
+            '`user` - Display a tracked user\'s stats\n' +
+            '`track` - Add a user and begin tracking their plays [MODERATORS ONLY]\n' +
+            '`untrack` - Remove a user and stop tracking their plays [MODERATORS ONLY]\n' +
+            '`tracklist` - Generate a .txt file containing all currently tracked users\n' +
+            '`submit` - Manually submit a score\n' +
+            '`unsubmit` - Remove a submitted score [MODERATORS ONLY]\n' +
+            '`scores` - Display the leaderboard for all scores set by tracked users\n' +
+            '`leaderboard` - Display the leaderboard for scores set by tracked users on a specific map\n')
       .setFooter({ text: 'https://github.com/mbalsdon/mor3' })
 
     await interaction.editReply({ embeds: [embed] })
